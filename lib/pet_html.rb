4510ccbcade5855c99838c0acc5838b66a0cd33f
luba.t <lubo4ka.boiko@gmail.com>
2020-11-16 14:48:53 +0200

    fixing

--- lib/pet_html.rb
+++ lib/pet_html.rb
@@ -4,21 +4,21 @@ class PetHtml
   end
   
   def make_html
-    content = File.read 'pet_html.html'
-    @html_new = 'pet_html_new.html'
-    File.open(@html.new, 'w') {|f|
+    content = File.read("#{self.class.root}/../pet_html.html")
+    @html_new = "#{self.class.root}/pet_html_new.html"
+    File.open(@html_new, 'w') {|f|
       f.puts content.gsub('{{name}}', @pet.name) 
-      f.puts content.gsub('{{response}}', @pet.response) 
-      f.puts content.gsub('{{mood}}', @pet.mood) 
-      f.puts content.gsub('{{states}}', @pet.states) 
-      f.puts content.gsub('{{number_of_lifes}}', @pet.lifes) 
-      f.puts content.gsub('{{number_of_lifes}}', @pet.emoji)     
+      f.puts content.gsub('{{response}}', @pet.response.join(',')) 
+      #f.puts content.gsub('{{mood}}', @pet.mood) 
+      #f.puts content.gsub('{{states}}', @pet.states) 
+      #f.puts content.gsub('{{number_of_lifes}}', @pet.lifes) 
+      #f.puts content.gsub('{{number_of_lifes}}', @pet.emoji)     
     }
   end
 
   def open_html
     make_html    
-    system("xdg-open pet_html.html")
+    system("xdg-open #{@html_new}")
   end
 
   def self.root
